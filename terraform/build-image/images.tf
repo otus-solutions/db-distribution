a7aaa8a9e538ca2dae4052b2d766bd40bd194d2f
Fernando Souza <fernandosdba@gmail.com>
2019-10-18 21:08:43 -0300

    OTUS-605 Alteração declaração de variaveis

--- terraform/build-image/images.tf
+++ terraform/build-image/images.tf
@@ -1,40 +1,50 @@
 ###############################################
 ###               Variables                 ###
 ###############################################
-variable "db-distribution-database" {
-  type = "map"
-  default = {
-    "name" = "db-distribution-database"
-    "directory" = "db-distribution"
-    "source" = "/source"
-  }
+variable "db-distribution-database-name" {
+  default = "db-distribution-database"
 }
-variable "db-distribution-api" {
-  type = "map"
-  default = {
-    "name" = "db-distribution-api"
-    "directory" = "db-distribution"
-    "source" = "/source"
-  }
+variable "db-distribution-database-directory" {
+  default = "db-distribution"
 }
-
+variable "db-distribution-database-source" {
+  default = "/source" 
+}
+variable "db-distribution-service-name" {
+  default = "db-distribution-service"
+}
+variable "db-distribution-service-directory" {
+  default = "db-distribution"
+}
+variable "db-distribution-service-source" {
+  default = "/source"
+}
+variable "db-distribution-service-npmbuild" {
+  default = "install"
+}
 ###############################################
 ###  DB-DISTRIBUTION : Build Image Database ###
 ###############################################
 resource "null_resource" "db-distribution-database" {
   provisioner "local-exec" {
-    command = "sudo docker build --target database -t ${var.db-distribution-database["name"]} ${var.db-distribution-database["directory"]}"
+    working_dir = "db-distribution"
+    command = "docker build --target database -t ${var.db-distribution-database-name} ."
   }
 }
 
 #################################################
 ### DB-DISTRIBUTION-API : Build Image Service ###
 #################################################
-resource "null_resource" "db-distribution-api" {
+resource "null_resource" "db-distribution-build" {
   provisioner "local-exec" {
-    command = "cd ${var.db-distribution-api["directory"]}/${var.db-distribution-api["source"]} && npm install"
+    working_dir = "db-distribution/source"
+    command = "npm ${var.db-distribution-service-npmbuild}"
   }
+}
+resource "null_resource" "db-distribution-service" {
+  depends_on = [null_resource.db-distribution-build]
   provisioner "local-exec" {
-    command = "sudo docker build --target api -t ${var.db-distribution-api["name"]} ${var.db-distribution-api["directory"]}"
+    working_dir = "db-distribution"
+    command = "docker build --target api -t ${var.db-distribution-service-name} ."
   }
 }
