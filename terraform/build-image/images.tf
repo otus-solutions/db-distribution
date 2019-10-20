adc896aa50a3c1726fdd4f99fe6af12d259bc27a
Diogo Ferreira <diogo@otus-solutions.com.br>
2019-10-19 23:55:58 -0300

    OTUS-605 Correções em variaveis

--- terraform/build-image/images.tf
+++ terraform/build-image/images.tf
@@ -8,7 +8,7 @@ variable "db-distribution-database-directory" {
   default = "db-distribution"
 }
 variable "db-distribution-database-source" {
-  default = "/source" 
+  default = "source" 
 }
 variable "db-distribution-service-name" {
   default = "db-distribution-service"
@@ -17,17 +17,19 @@ variable "db-distribution-service-directory" {
   default = "db-distribution"
 }
 variable "db-distribution-service-source" {
-  default = "/source"
+  default = "source"
 }
-variable "db-distribution-service-npmbuild" {
-  default = "install"
+variable "db-distribution-service-npminstall" {
+  default = "install --production"
+}
+variable "db-distribution-service-npmtest" {
+  default = "test"
 }
 ###############################################
 ###  DB-DISTRIBUTION : Build Image Database ###
 ###############################################
 resource "null_resource" "db-distribution-database" {
   provisioner "local-exec" {
-    working_dir = "db-distribution"
     command = "docker build --target database -t ${var.db-distribution-database-name} ."
   }
 }
@@ -35,16 +37,23 @@ resource "null_resource" "db-distribution-database" {
 #################################################
 ### DB-DISTRIBUTION-API : Build Image Service ###
 #################################################
-resource "null_resource" "db-distribution-build" {
+resource "null_resource" "db-distribution-install" {
   provisioner "local-exec" {
-    working_dir = "db-distribution/source"
-    command = "npm ${var.db-distribution-service-npmbuild}"
+    working_dir = "${var.db-distribution-service-source}"
+    command = "npm ${var.db-distribution-service-npminstall}"
+  }
+}
+
+resource "null_resource" "db-distribution-test" {
+  depends_on = [null_resource.db-distribution-install]
+  provisioner "local-exec" {
+    working_dir = "${var.db-distribution-service-source}"
+    command = "npm ${var.db-distribution-service-npmtest}"
   }
 }
 resource "null_resource" "db-distribution-service" {
-  depends_on = [null_resource.db-distribution-build]
+  depends_on = [null_resource.db-distribution-test]
   provisioner "local-exec" {
-    working_dir = "db-distribution"
     command = "docker build --target api -t ${var.db-distribution-service-name} ."
   }
 }
--- terraform/run-container/containers.tf
+++ terraform/run-container/containers.tf
@@ -1,24 +1,28 @@
-variable "db-distribution-database" {
-  type = "map"
-  default = {
-	"name" = "db-distribution-database"
-	"persistence-directory" = "/home/drferreira/otus-platform/docker-persistence/db-distribution/database"
-	"port" = 53001
-  }
+variable "db-distribution-database-persistence"{
+  default = "/otus-platform/docker-persistence/db-distribution/database"
 }
-variable "db-distribution-service" {
-  type = "map"
-  default = {
-	"name" = "db-distribution-service"
-	"port" = 53002
-  }
+
+variable "db-distribution-database-port"{
+  default = 53001
+}
+
+variable "db-distribution-service-port"{
+  default = 53002
+}
+
+variable "db-distribution-service-version"{
+  default = "latest"
+}
+
+variable "db-distribution-database-version"{
+  default = "latest"
 }
 
 resource "docker_image" "db-distribution-database" {
-  name = "db-distribution-database:latest"
+  name = "db-distribution-database:${var.db-distribution-database-version}"
 }
 resource "docker_image" "db-distribution-service" {
-  name = "db-distribution-service:latest"
+  name = "db-distribution-service:${var.db-distribution-service-version}"
 }
 
 resource "docker_network" "db-distribution-network"{
@@ -27,13 +31,13 @@ resource "docker_network" "db-distribution-network"{
 
 resource "docker_container" "db-distribution-database" {
   name = "db-distribution-database"
-  image = "${docker_image.db-distribution-database.latest}"
+  image = "${docker_image.db-distribution-database.name}"
   ports {
 	internal = 27017
-	external = "${var.db-distribution-database["port"]}"
+	external = "${var.db-distribution-database-port}"
   }
   volumes {
-	host_path = "${var.db-distribution-database["persistence-directory"]}"
+	host_path = "${var.db-distribution-database-persistence}"
 	container_path = "/data/db"
   }
   networks_advanced {
@@ -43,10 +47,10 @@ resource "docker_container" "db-distribution-database" {
 resource "docker_container" "db-distribution-service" {
   depends_on = [docker_container.db-distribution-database]
   name = "db-distribution-service"
-  image = "${docker_image.db-distribution-service.latest}"
+  image = "${docker_image.db-distribution-service.name}"
   ports {
 	internal = 8080
-	external = "${var.db-distribution-service["port"]}"
+	external = "${var.db-distribution-service-port}"
   }
   networks_advanced {
     name = "${docker_network.db-distribution-network.name}"
