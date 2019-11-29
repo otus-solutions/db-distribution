75973c51b0c8b18a2fa8189d725fc84f1b268b7c
emanoelvianna <emanoel.vianna@outlook.com>
2019-11-29 11:56:51 -0300

    OOD-3 - estrutura para os testes de unidade

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,4 +1,18 @@
-(function() {
-  var requireDir = require('require-dir');
-  requireDir('./gulp-tasks');
-}());
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    proxy: "localhost:8080",
+    reloadDelay: 400,
+    files: [
+      "app/views/*/*.ejs",
+      "app/routes/*.js",
+      "app/models/*.js",
+      "app/controllers/*.js",
+      "config/*.js"
+    ],
+    open: "external"
+  });
+});
