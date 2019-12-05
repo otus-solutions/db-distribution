eca01694aa9e71b121aa3eb1a5f362ee6fc118fe
Fabiano Ramires <fdrtec@gmail.com>
2019-12-05 15:33:46 -0300

    OOD-3 estrutura inicial do ctrl de staticDataBase

--- source/test/controllers/StaticDatabaseController-spec.js
+++ source/test/controllers/StaticDatabaseController-spec.js
@@ -1,13 +1,15 @@
 describe('StaticDatabaseController.js Tests', function () {
-  var app, assert, chai, expect;
+  let application, assert, chai, expect;
 
   beforeEach(function () {
-    app = require("../../app/controllers/StaticDatabaseController.js");
+    application = require("../../config/server");
+
+    ctrl = require("../../app/controllers/StaticDatabaseController.js")(application);
   });
 
   describe('', function () {
     it('', function () {
-      console.log(app.getVariables());
+      console.log(ctrl)
     });
   });
 });
