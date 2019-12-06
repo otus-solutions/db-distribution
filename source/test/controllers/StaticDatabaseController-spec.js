6f1bb8e424bc9ff9d14aee2460753c42037e58c2
emanoelvianna <emanoel.vianna@outlook.com>
2019-12-06 11:41:42 -0300

    OOD-3 - continuação dos testes de unidade

--- source/test/controllers/StaticDatabaseController-spec.js
+++ source/test/controllers/StaticDatabaseController-spec.js
@@ -1,15 +1,31 @@
-describe('StaticDatabaseController.js Tests', function () {
-  let application, assert, chai, expect;
+xdescribe('StaticDatabaseController.js Tests', function () {
+  var ctrl;
+  var Mock = {};
+  var Injections = [];
+
+  const sandbox = require('sinon').createSandbox()
+  const expect = require('chai').expect;
+  const sinon = require('sinon');
 
   beforeEach(function () {
     application = require("../../config/server");
 
     ctrl = require("../../app/controllers/StaticDatabaseController.js")(application);
+
   });
 
-  describe('', function () {
-    it('', function () {
-      console.log(ctrl)
+  describe('getVariables method', function () {
+    var setNameSpy;
+    afterEach(() => {
+      sandbox.restore()
+    });
+
+    it('shoul call getVariables method from StaticDatabaseService', function () {
+      sandbox.stub(ctrl, 'getVariables').returns({});
+      setNameSpy = sinon.spy(user, 'setName');
+
+      ctrl.getVariables();
     });
   });
+
 });
