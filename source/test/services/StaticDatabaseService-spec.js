89948c5318f4fc37f01397afa7253be4af88d1a3
Fabiano Ramires <fdrtec@gmail.com>
2019-12-10 13:14:43 -0300

    ODD-3 test com cenário de exception para getVariable de StaticDatabaseService

--- source/test/services/StaticDatabaseService-spec.js
+++ source/test/services/StaticDatabaseService-spec.js
@@ -1,14 +1,31 @@
 describe('StaticDatabaseService.js Tests', () => {
-    let application, assert, chai, expect;
+    let service, application;
 
-    beforeEach(function () {
+    const assert = require('assert');
+    const chai = require('chai');
+    const expect = chai.expect;
+    const fs = require('fs');
+
+    beforeEach(() => {
         application = require("../../config/server");
         service = require("../../app/services/StaticDatabaseService.js")(application);
     });
 
-    describe('', function () {
-        it('', function () {
-            console.log(service)
-        });
+    it('serviceExistence_check', () => {
+        expect(service).not.to.be.undefined;
     });
+
+    it('serviceMethodsExistence_check', () => {
+        expect(service.uploadDatabase).not.to.be.undefined;
+        expect(service.getVariables).not.to.be.undefined;
+    });
+
+
+    it('uploadDatabase_method_should', (done) => {
+        let databaseCSV = {path: '{"ab":1, "bc":2}'}
+        assert.equal((1 + 1), 2)
+        console.log(service.uploadDatabase(databaseCSV))
+        done();
+    });
+
 });
